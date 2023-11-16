24938a6dc24c6fc6c991b533d5618d79979d4cf5
nato <solrock12@gmail.com>
2023-11-15 22:23:58 -0300

    Initial commit

---
+++ src/test/java/com/devDream/Practica/DevDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devDream.Practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
